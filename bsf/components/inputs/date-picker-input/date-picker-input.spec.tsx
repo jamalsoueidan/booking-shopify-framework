e42804ec29974c3ad977d359ddc75c4ce571c3d5
Jamal Soueidan <jamal@soueidan.com>
2023-01-23 02:55:00 +0300

    delete spec

--- bsf/components/inputs/date-picker-input/date-picker-input.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@jamalsoueidan/bsd.testing-library.react";
-import { BasicDatePickerInput } from "./date-picker-input.composition";
-
-it("should render with the correct text", () => {
-  const { getByText } = render(<BasicDatePickerInput />);
-  const rendered = getByText("Date");
-  expect(rendered).toBeTruthy();
-});
