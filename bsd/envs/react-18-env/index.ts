5a81c7e47cdcd8094f6c38605fb2a09621c4c6c2
Jamal Soueidan <jamal@soueidan.com>
2023-01-30 18:05:16 +0300

    remove react-18 env

--- bsd/envs/react-18-env/index.ts
+++
@@ -1,5 +0,0 @@
-import { React18EnvAspect } from './react-18-env.aspect';
-
-export type { React18EnvMain } from './react-18-env.main.runtime';
-export default React18EnvAspect;
-export { React18EnvAspect };
--- bsd/envs/react-18-env/react-18-env.aspect.ts
+++
@@ -1,5 +0,0 @@
-import { Aspect } from '@teambit/harmony';
-
-export const React18EnvAspect = Aspect.create({
-  id: 'jamalsoueidan.bsd/react-version-envs/react-18-env',
-});
